352c011db2d9d200ce314cb18d0db4037c45aebc
Basavraj Birajdar <basavrajbirajdar1831@gmail.com>
2025-03-01 19:49:53 +0530

    commit

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import { GoogleLogin, GuestLogin, Login, Logout, Register, resetPassword, sendResetOtp, verifyEmail } from '../controllers/authController.js'
+import { GoogleLogin, Login, Logout, Register, resetPassword, sendResetOtp, verifyEmail } from '../controllers/authController.js'
 
 const AuthRoute = express.Router()
 
 AuthRoute.post('/register', Register);
 AuthRoute.post('/login', Login);
 AuthRoute.post('/google-login', GoogleLogin);
-AuthRoute.post('/guest-login', GuestLogin)
 AuthRoute.post('/verify-email', verifyEmail);
 AuthRoute.post('/send-reset-otp', sendResetOtp);
 AuthRoute.post('/reset-password', resetPassword);
